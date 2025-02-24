45c49027a2f7dd1650305a1b4173c73d8b45a39e
aqyuki <aqyuki@aqyuki.dev>
2025-02-25 07:31:15 +0900

    🐛 Fix the problem that failed to send notice to Discord webhook

--- cron.sh
+++ cron.sh
@@ -19,7 +19,7 @@ function send_webhook_notice() {
     -d "$(jq --arg title "Notice" \
              --arg description "$(date -d '5 minutes' +'%Y-%m-%d %H:%M:%S')より､サーバーの再起動とバックアップを行います｡" \
              '.embeds[0].title = $title | .embeds[0].description = $description' \
-             asset/message.json)"
+             "$SCRIPT_DIR"/asset/message.json)"
 }
 
 function send_webhook_stop() {
@@ -28,7 +28,7 @@ function send_webhook_stop() {
     -d "$(jq --arg title "Server stop" \
              --arg description "サーバーが停止しました｡" \
              '.embeds[0].title = $title | .embeds[0].description = $description' \
-             asset/message.json)"
+             "$SCRIPT_DIR"asset/message.json)"
 }
 
 function send_webhook_start() {
@@ -37,7 +37,7 @@ function send_webhook_start() {
     -d "$(jq --arg title "Server start" \
              --arg description "サーバーが再起動しました｡" \
              '.embeds[0].title = $title | .embeds[0].description = $description' \
-             asset/message.json)"
+             "$SCRIPT_DIR"asset/message.json)"
 }
 
 function send_backup_result() {
@@ -46,7 +46,7 @@ function send_backup_result() {
     -d "$(jq --arg title "Backup complete" \
              --arg description "バックアップが完了しました｡" \
              '.embeds[0].title = $title | .embeds[0].description = $description' \
-             asset/message.json)"
+             "$SCRIPT_DIR"asset/message.json)"
 }
 
 # Main script execution
