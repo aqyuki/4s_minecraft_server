4de8b9182222036bd2b7cc96c6bc103dd4925dea
aqyuki <aqyuki@aqyuki.dev>
2025-02-25 07:07:43 +0900

    Revert "fix cron script"

--- cron.sh
+++ cron.sh
@@ -9,53 +9,53 @@ readonly SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # Global utility functions
 function printl() {
-  /usr/bin/echo "[$(date)] $1"
+  echo "[$(date)] $1"
 }
 
 # Send webhook
 function send_webhook_notice() {
-  /usr/bin/curl -X POST "$WEBHOO_URL" \
+  curl -X POST "$WEBHOO_URL" \
     -H 'Content-Type: application/json' \
-    -d "$(/usr/bin/jq --arg title "Notice" \
-             --arg description "$(/usr/bin/date -d '5 minutes' +'%Y-%m-%d %H:%M:%S')より､サーバーの再起動とバックアップを行います｡" \
+    -d "$(jq --arg title "Notice" \
+             --arg description "$(date -d '5 minutes' +'%Y-%m-%d %H:%M:%S')より､サーバーの再起動とバックアップを行います｡" \
              '.embeds[0].title = $title | .embeds[0].description = $description' \
              asset/message.json)"
 }
 
 function send_webhook_stop() {
-  /usr/bin/curl -X POST "$WEBHOO_URL" \
+  curl -X POST "$WEBHOO_URL" \
     -H 'Content-Type: application/json' \
-    -d "$(/usr/bin/jq --arg title "Server stop" \
+    -d "$(jq --arg title "Server stop" \
              --arg description "サーバーが停止しました｡" \
              '.embeds[0].title = $title | .embeds[0].description = $description' \
              asset/message.json)"
 }
 
 function send_webhook_start() {
-  /usr/bin/curl -X POST "$WEBHOO_URL" \
+  curl -X POST "$WEBHOO_URL" \
     -H 'Content-Type: application/json' \
-    -d "$(/usr/bin/jq --arg title "Server start" \
+    -d "$(jq --arg title "Server start" \
              --arg description "サーバーが再起動しました｡" \
              '.embeds[0].title = $title | .embeds[0].description = $description' \
              asset/message.json)"
 }
 
 function send_backup_result() {
-  /usr/bin/curl -X POST "$WEBHOO_URL" \
+  curl -X POST "$WEBHOO_URL" \
     -H 'Content-Type: application/json' \
-    -d "$(/usr/bin/jq --arg title "Backup complete" \
+    -d "$(jq --arg title "Backup complete" \
              --arg description "バックアップが完了しました｡" \
              '.embeds[0].title = $title | .embeds[0].description = $description' \
              asset/message.json)"
 }
 
 # Main script execution
-$SCRIPT_DIR/mc.sh msg "§9[Server]§r $(/usr/bin/date -d "5 minutes" +"%Y-%m-%d %H:%M")より､サーバーの再起動とバックアップを開始します｡"
+./mc.sh msg "§9[Server]§r $(date -d "5 minutes" +"%Y-%m-%d %H:%M")より､サーバーの再起動とバックアップを開始します｡"
 send_webhook_notice
 sleep 5m
 
 printl "Stopping server"
-if $SCRIPT_DIR/mc.sh stop > /dev/null; then
+if ./mc.sh stop > /dev/null; then
   printl "Server stopped successfully"
   send_webhook_stop
 else
@@ -64,7 +64,7 @@ else
 fi
 
 printl "Starting backup process"
-if $SCRIPT_DIR/backup.sh; then
+if ./backup.sh; then
   printl "Backup completed successfully."
   send_backup_result
 else
@@ -73,7 +73,7 @@ else
 fi
 
 printl "Backup completed successfully. Restarting server"
-if $SCRIPT_DIR/mc.sh start > /dev/null; then
+if ./mc.sh start; then
   printl "Server restarted successfully"
   send_webhook_start
 else
--- mc.sh
+++ mc.sh
@@ -5,54 +5,54 @@
 set -e
 
 # Global variables
-readonly SCRIPT_DIR="$(cd "$(/usr/bin/dirname "$0")" && /usr/bin/pwd)"
+readonly SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 readonly SERVER_DIR="$SCRIPT_DIR/server"
 readonly SESSION_NAME="minecraft"
 
 # Start the Minecraft server
 start_server() {
-  if ! /usr/bin/tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
-    /usr/bin/echo "tmux session ($SESSION_NAME) not found. Creating a new session and starting the Minecraft server..."
-    /usr/bin/tmux new-session -d -s "$SESSION_NAME"
-    /usr/bin/tmux send-keys -t "$SESSION_NAME" "cd \"$SERVER_DIR\" && LD_LIBRARY_PATH=. ./bedrock_server" C-m
-    /usr/bin/echo "Server started."
+  if ! tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
+    echo "tmux session ($SESSION_NAME) not found. Creating a new session and starting the Minecraft server..."
+    tmux new-session -d -s "$SESSION_NAME"
+    tmux send-keys -t "$SESSION_NAME" "cd \"$SERVER_DIR\" && LD_LIBRARY_PATH=. ./bedrock_server" C-m
+    echo "Server started."
   else
-    /usr/bin/echo "Server is already running."
+    echo "Server is already running."
     exit 1
   fi
 }
 
 # Stop the Minecraft server
 stop_server() {
-  if /usr/bin/tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
-    /usr/bin/echo "Stopping the Minecraft server..."
-    /usr/bin/tmux send-keys -t "$SESSION_NAME" "stop" C-m
-    /usr/bin/tmux kill-session -t "$SESSION_NAME"
-    /usr/bin/echo "Server stopped."
+  if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
+    echo "Stopping the Minecraft server..."
+    tmux send-keys -t "$SESSION_NAME" "stop" C-m
+    tmux kill-session -t "$SESSION_NAME"
+    echo "Server stopped."
   else
-    /usr/bin/echo "Server is not running."
+    echo "Server is not running."
     exit 1
   fi
 }
 
 # Send message to the Minecraft server
 send_message() {
-  if /usr/bin/tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
-    /usr/bin/echo "Sending message to the Minecraft server..."
-    /usr/bin/tmux send-keys -t "$SESSION_NAME" "say $1" C-m
-    /usr/bin/echo "Message sent: $1"
+  if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
+    echo "Sending message to the Minecraft server..."
+    tmux send-keys -t "$SESSION_NAME" "say $1" C-m
+    echo "Message sent: $1"
   else
-    /usr/bin/echo "Server is not running. Cannot send message."
+    echo "Server is not running. Cannot send message."
     exit 1
   fi
 }
 
 # Attach to the Minecraft server console
 attach_server() {
-  if /usr/bin/tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
-    /usr/bin/tmux attach -t "$SESSION_NAME"
+  if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
+    tmux attach -t "$SESSION_NAME"
   else
-    /usr/bin/echo "Server is not running."
+    echo "Server is not running."
     exit 1
   fi
 }
@@ -60,7 +60,7 @@ attach_server() {
 
 # Display usage information
 show_help() {
-  /usr/bin/echo "Usage: $0 {start|stop|help|restart|msg|attach}"
+  echo "Usage: $0 {start|stop|help}"
 }
 
 # Handle script arguments
